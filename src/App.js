efd726374a8be747b66aa756cf151cfadea7c900
datu1210 <turizod@gmail.com>
2021-09-21 09:20:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import molino from './App-logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className='App-logo' src={molino} alt = 'molino'/>
+        <h1>ESTA ES MY APP</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
